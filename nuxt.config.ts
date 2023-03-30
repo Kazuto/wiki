f71d7095f6d37cc57384f5606509060a271a074d
Kazuto <mail@kazuto.de>
2023-03-30 14:51:25 +0200

    feat(config): switch to document driven

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/content'],
+  content: {
+    documentDriven: true,
+  },
   css: ['~/assets/css/app.css'],
   components: [{ path: '~/components', prefix: 'v' }],
   postcss: {
