8a8d494f79b308cdfd969afb06c02ed0d91e7beb
Kazuto <mail@kazuto.de>
2023-03-30 13:15:56 +0200

    feat(node): add tailwindcss, postcss and autoprefixer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content'
-  ],
-})
+  modules: ['@nuxt/content'],
+  css: ['~/assets/css/app.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
