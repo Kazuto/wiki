8476b94955965429f73eecbd2a88a714297a200b
Kai Mayer <mail@kazuto.de>
2023-03-30 15:44:09 +0200

    Base Layout (#1)
    * feat(style): base layout
    
    * feat(config): prefix components with "v"
    
    * feat(style): extract header and footer
    
    * feat(dev): toggle for dark mode
    
    * feat(style): add base sidebar component
    
    * feat(config): switch to document driven
    
    * feat(style): add base content sidebar component
    
    * feat(style): prev and next links

---
+++ components/layout/footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="h-14 bg-neutral-50 dark:bg-neutral-600 p-2 text-sm">
+    <vContainer class="flex items-center justify-between">
+      Some content
+    </vContainer>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/content'],
+  content: {
+    documentDriven: true,
+  },
   css: ['~/assets/css/app.css'],
+  components: [{ path: '~/components', prefix: 'v' }],
   postcss: {
     plugins: {
       tailwindcss: {},
