92fb1bfa3dbc998cd3e2eee529c9528a7f99b65f
Kazuto <mail@kazuto.de>
2023-04-06 15:35:08 +0200

    feat(component): add node tree and item for navigation

---
+++ components/navigation/node/item.vue
@@ -0,0 +1,52 @@
+<template>
+  <template v-if="isNavItem(item)">
+    <h4
+      v-if="item.children"
+      class="uppercase mt-2"
+    >
+      {{ item.title }}
+    </h4>
+
+    <NuxtLink
+      v-else
+      :to="item._path"
+      class="block my-1 text-sm transition-colors hover:text-primary-500 dark:hover:text-primary-200"
+    >
+      {{ item.title }}
+    </NuxtLink>
+  </template>
+
+  <template v-else>
+    <a
+      :href="`#${item.id}`"
+      class="block mb-1 transition-colors hover:text-primary-500 dark:hover:text-primary-200"
+    >
+      {{ item.text }}
+    </a>
+  </template>
+</template>
+
+<script setup lang="ts">
+import { NavItem, TocLink } from '@nuxt/content/dist/runtime/types';
+
+defineProps({
+  item: {
+    type: Object as PropType<NavItem | TocLink>,
+    default: () => {},
+  },
+});
+
+const isNavItem = (item: NavItem | TocLink): item is NavItem => {
+  return (<NavItem>item)._path !== undefined;
+};
+</script>
+
+<style lang="postcss">
+.router-link-active {
+  @apply text-primary-500 font-medium;
+}
+
+.router-link-exact-active {
+  @apply text-primary-400 font-medium;
+}
+</style>
---
+++ components/navigation/node/tree.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="mb-3">
+    <li
+      v-for="(item, index) in node"
+      :key="index"
+    >
+      <VNavigationNodeItem :item="item" />
+
+      <vNavigationNodeTree
+        v-if="item.children"
+        :node="item.children"
+        class="pl-2"
+        :level="level + 1"
+      />
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { NavItem, TocLink } from '@nuxt/content/dist/runtime/types';
+
+defineProps({
+  node: {
+    type: Array as PropType<NavItem[] | TocLink[]>,
+    default: () => [],
+  },
+  level: {
+    type: Number,
+    default: 1,
+  },
+});
+</script>
+
+<style lang="postcss" scoped></style>
