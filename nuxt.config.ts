ff7bc1768b461ad62d4b09201cd459abda91bd51
Kazuto <mail@kazuto.de>
2023-04-06 15:34:07 +0200

    feat(config): add tailwind nesting and colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
   components: [{ path: '~/components', prefix: 'v' }],
   postcss: {
     plugins: {
+      'tailwindcss/nesting': {},
       tailwindcss: {},
       autoprefixer: {},
     },
