ca89152a2d0ce1134e05afc92bfb9842bed2ba0b
Kazuto <mail@kazuto.de>
2023-07-11 12:37:16 +0200

    refactor(ui): change colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 module.exports = {
   darkMode: 'class',
   content: [
@@ -10,35 +12,40 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Nunito Sans"', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         gray: {
-          DEFAULT: '#514158',
-          50: '#DCD0DB',
-          100: '#CFBECF',
-          200: '#B49BB5',
-          300: '#97789B',
-          400: '#755B7B',
-          500: '#514158',
-          600: '#3D3243',
-          700: '#29232F',
-          800: '#17141A',
-          900: '#050406',
+          DEFAULT: '#1D1C1D',
+          50: '#F5F5F5',
+          100: '#E9E8E9',
+          200: '#D0CED0',
+          300: '#B7B4B7',
+          400: '#9E9A9E',
+          500: '#858085',
+          600: '#6B676B',
+          700: '#514E51',
+          800: '#373537',
+          900: '#1D1C1D',
+          950: '#100F10',
         },
         primary: {
-          DEFAULT: '#7E2A9F',
-          50: '#F8E4F6',
-          100: '#F2CCF0',
-          200: '#E29CE5',
-          300: '#CB6CD8',
-          400: '#AE3BCB',
-          500: '#7E2A9F',
-          600: '#602383',
-          700: '#451B66',
-          800: '#2D144A',
-          900: '#190C2E',
+          DEFAULT: '#FF9900',
+          50: '#FFD08A',
+          100: '#FFCA7A',
+          200: '#FFBE5C',
+          300: '#FFB13D',
+          400: '#FFA51F',
+          500: '#FF9900',
+          600: '#E68A00',
+          700: '#CC7A00',
+          800: '#B36B00',
+          900: '#995C00',
+          950: '#8C5400',
         },
       },
     },
   },
   plugins: [],
-};
+}
