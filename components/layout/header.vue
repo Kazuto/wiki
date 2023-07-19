e47c1269247ebd65a062dd955e8d9b6eb41fbcea
Kazuto <mail@kazuto.de>
2023-07-19 14:08:09 +0200

    feat(header): sticky header on smaller screens

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,6 +1,8 @@
 <template>
-  <header class="h-14 border-b border-gray-100 px-16 dark:border-gray-800">
-    <vContainer class="flex items-center justify-between p-4">
+  <header
+    class="sticky top-0 h-14 border-b border-gray-100 bg-gray-50 px-8 text-gray-600 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-100 lg:static xl:px-16"
+  >
+    <vContainer class="flex items-center justify-between">
       <span></span>
       <vThemeSwitch />
     </vContainer>
