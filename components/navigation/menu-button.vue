5f472e6e2f26863dda07272d5a482374b0175fc6
Kazuto <mail@kazuto.de>
2023-07-20 12:09:56 +0200

    fix(buttons): bind $attrs to button
    ClientOnly resulted in not binding the attributes to the button anymore

--- components/navigation/menu-button.vue
+++ components/navigation/menu-button.vue
@@ -1,6 +1,7 @@
 <template>
   <ClientOnly>
     <button
+      v-bind="$attrs"
       class="flex h-10 w-10 items-center justify-center rounded text-2xl transition hover:bg-gray-100 dark:hover:bg-gray-800"
       :aria-label="menuState ? 'Close menu' : 'Open menu'"
       @click.prevent="() => (menuState = !menuState)"
